f257d07bcf251c20353461c55ecc3b64d5cf6e83
oxo <samuelesantonicola@gmail.com>
2024-05-27 16:09:29 +0200

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name="Wuthering Waves FPS Unlock",
-    version="1.1",
+    version="1.0",
     description="A tool to change FPS",
     options={'build_exe': {'include_files': ['icon.ico']}},
     executables=[Executable("WW_Unlocker.py", icon="icon.ico", base="Win32GUI")]
